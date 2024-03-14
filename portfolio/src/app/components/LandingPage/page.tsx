8445f424862d199708d8ce2ea61ea56ab354b22b
Faiz Mustansar <faizmustansar10@gmail.com>
2024-03-14 14:21:14 -0400

    Update page.tsx

--- portfolio/src/app/components/LandingPage/page.tsx
+++ portfolio/src/app/components/LandingPage/page.tsx
@@ -1,28 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Navbar from "../NavBar/page";
-
+// 
 const LandingPage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar /> {/* Use the Navbar component */}
       <div
         className="flex-1 flex items-center justify-center flex-col text-center"
-        // style={{
-        //     background: 'linear-gradient(to bottom, #00aaff, #0077cc)', // Example gradient from light blue to a darker blue
-        //     backgroundSize: 'cover',
-        //     backgroundPosition: 'center'
-        //   }}
         style={{
-            backgroundImage: 'url(/images/background1.webp)',
-            backgroundPosition: 'center '
-        }
-        }
+            backgroundColor: 'black', // Example gradient from light blue to a darker blue
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+        // style={{
+        //     backgroundImage: 'url(/images/background1.webp)',
+        //     backgroundPosition: 'center '
+        // }
+        // }
       >
-        <h1 className="text-5xl font-bold mb-6">
+        {/* <h1 className="text-5xl font-bold mb-6">
           Hey there! I am Faiz M and I am a software developer
-        </h1>
+        </h1> */}
         {/* enter text below */}
         <p className="mb-6"> 
         </p>
@@ -35,16 +33,24 @@ const LandingPage: React.FC = () => {
         <div className="mt-6">
           <Image
             src="/images/profilepic.webp"
-            alt="Full Stack Web Developer"
+            alt="Software Developer"
             width={200}
             height={200}
+            className="rounded-full object-cover"
           />
         </div>
+         <h1 className="text-2xl font-bold mb-2">
+          Faiz Mustansar
+        </h1> 
+        <h1 className="text-medium font-bold mb-6">
+          Software Developer
+        </h1> 
+        
       </div>
       {/* Additional page sections would go here... */}
-      <footer className="bg-blue-800 p-4 text-center">
+      {/* <footer className="bg-blue-800 p-4 text-center">
         © {new Date().getFullYear()} Faiz Mustansar
-      </footer>
+      </footer> */}
     </div>
   );
 };
