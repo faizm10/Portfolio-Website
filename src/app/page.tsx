e87fbdeebab9b27035f7d4d3417007226c2b7fd4
faizm10 <faizmustansar10@gmail.com>
2024-03-14 15:11:09 -0400

    testing

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+// src/app/page.tsx
+
+import React from 'react';
+import LandingPage from './components/LandingPage/page';
+//import AboutPage from './components/About/page';
+//import Footer from './components/Footer/page';
+import Navbar from './components/NavBar/page';
+const Page: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <LandingPage />
+    </>
+  );
+};
+
+export default Page;
