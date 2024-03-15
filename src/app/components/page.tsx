b5a33ec775e3b028779ce60ad04296ea45f707f8
Faiz Mustansar <faizmustansar10@gmail.com>
2024-03-15 17:00:15 -0400

    Update page.tsx

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -5,19 +5,3 @@ import AboutMeSection from "./About/page";
 import Projects from "./Projects/page";
 import Footer from "./Footer/page";
 import Experience from "./Experience/page";
-
-const MainPage = () => {
-  return (
-    <div>
-      <Navbar />
-      <LandingPage />
-      <AboutMeSection />
-      <Experience />
-      <Projects />
-      <ContactMeSection />
-      <Footer />
-    </div>
-  );
-};
-
-export default MainPage;
