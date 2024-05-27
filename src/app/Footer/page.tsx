5a0dfbb80c76b51941731a117f23a50873149136
faizm10 <faizmustansar10@gmail.com>
2024-05-27 19:13:22 -0400

    edited footer

--- src/app/Footer/page.tsx
+++ src/app/Footer/page.tsx
@@ -1,69 +1,24 @@
 import React from "react";
-import Link from "next/link";
+import { Link } from "@nextui-org/react";
 
 const Footer: React.FC = () => {
   return (
     <footer
-      className="w-full text-white py-4 bg-white"
-      style={{
-        // background: "linear-gradient(to bottom, #feb47b, #3573cf)",
-        position: "static",
-        bottom: 0,
-      }}
+      className="bg-white text-black p-4"
+      style={{ fontFamily: "sans-serif" }}
     >
-      <div className="flex justify-center items-center space-x-4">
-        <Link href="https://github.com/faizm10">
-          <span
-            rel="noopener noreferrer"
-            aria-label="GitHub"
-            className="hover:opacity-75 transition-opacity duration-300"
-          >
-            <img
-              src="/images/socials/github.png"
-              alt="GitHub"
-              className="w-8 h-8"
-            />
-          </span>
-        </Link>
-        <Link href="https://www.linkedin.com/in/faiz-mustansar-a9a435213/">
-          <span
-            rel="noopener noreferrer"
-            aria-label="LinkedIn"
-            className="hover:opacity-75 transition-opacity duration-300"
-          >
-            <img
-              src="/images/socials/linkedin.png"
-              alt="LinkedIn"
-              className="w-8 h-8"
-            />
-          </span>
-        </Link>
-        <Link href="https://www.instagram.com/_faiz_m_10/">
-          <span
-            rel="noopener noreferrer"
-            aria-label="Instagram"
-            className="hover:opacity-75 transition-opacity duration-300"
-          >
-            <img
-              src="/images/socials/instagram.png"
-              alt="Instagram"
-              className="w-8 h-8"
-            />
-          </span>
-        </Link>
-        <Link href="mailto:faizmustansar10@gmail.com">
-          <span
-            rel="noopener noreferrer"
-            aria-label="Gmail"
-            className="hover:opacity-75 transition-opacity duration-300"
-          >
-            <img
-              src="/images/socials/gmail.png"
-              alt="Gmail"
-              className="w-8 h-8"
-            />
-          </span>
-        </Link>
+      <div className="container mx-auto flex justify-between items-center flex-wrap">
+        <div>
+          <Link isExternal href="https://faizm10.github.io/" showAnchorIcon>
+            Old Portfolio Site
+          </Link>
+        </div>
+        <div className="flex space-x-4">
+          <p className="text-md">Copyright © 2024 Faiz Mustansar</p>
+        </div>
+        {/* <div>
+        
+        </div> */}
       </div>
     </footer>
   );
