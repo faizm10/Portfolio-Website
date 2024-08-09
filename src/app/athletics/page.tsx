40eeca489846e1bdc7c998712043563209e6c5a5
faizm10 <faizmustansar10@gmail.com>
2024-08-09 15:02:04 -0400

    added photography page UI and pics

---
+++ src/app/athletics/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AthleticsPage = () => {
+  return (
+    <>
+      <div
+        className="flex flex-col items-center h-full w-full px-6 py-8"
+        style={{ backgroundColor: "#fdddb7" }}
+      >
+        <h1 className="text-5xl font-bold mb-12 text-center">Athletics</h1>
+      </div>
+    </>
+  );
+};
+
+export default AthleticsPage;
